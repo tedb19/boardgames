cfb4132adf74ec80092549c080b33570756d88d6
tedb19 <tedb19@gmail.com>
2015-07-14 05:52:11 +0300

    Added the homepage app

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'boardgames.views.home', name='home'),
+    url(r'^$', 'main.views.home', name='home'),
     # url(r'^boardgames/', include('boardgames.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
